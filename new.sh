8f0b4ccc90987f0ce36145ac4c72e38de3491f2f
ZedSoftOfficial <132583827+ZedSoftOfficial@users.noreply.github.com>
2024-08-24 15:25:42 +0330

    Update new.sh

--- new.sh
+++ new.sh
@@ -198,31 +198,38 @@ case $server_choice in
         read -p "Select an option (1-3): " server_option
 
         if [ "$server_option" -eq 1 ]; then
-            # اجرای 6to4 multi server برای خارج از ایران
-            read -p "Enter the IP outside: " ipkharej1
-            read -p "Enter the IP Iran1: " ipiran1
-            read -p "Enter the IP Iran2: " ipiran2
+        # برای سرور Outside
+        read -p "Enter the IP Outside: " ipkharej1
+        read -p "Enter the IP Iran1: " ipiran1
+        read -p "Enter the IP Iran2: " ipiran2
 
-            commands=$(cat <<EOL
+        # ایجاد محتوای جدید برای فایل rc.local
+        cat <<EOL > /etc/rc.local
+#!/bin/bash
+
+# تنظیمات تونل برای اولین سرور ایران
 ip tunnel add 6to4_To_IR1 mode sit remote $ipiran1 local $ipkharej1
 ip -6 addr add 2002:480:1f10:e1f::2/64 dev 6to4_To_IR1
 ip link set 6to4_To_IR1 mtu 1480
 ip link set 6to4_To_IR1 up
 
-ip -6 tunnel add GRE6Tun_To_IR1 mode ip6gre remote 2002:480:1f10:e1f::4 local 2002:480:1f10:e1f::2
+ip -6 tunnel add GRE6Tun_To_IR1 mode ip6gre remote 2002:480:1f10:e1f::1 local 2002:480:1f10:e1f::2
 ip addr add 10.10.10.2/30 dev GRE6Tun_To_IR1
 ip link set GRE6Tun_To_IR1 mtu 1436
 ip link set GRE6Tun_To_IR1 up
 
+# تنظیمات تونل برای دومین سرور ایران
 ip tunnel add 6to4_To_IR2 mode sit remote $ipiran2 local $ipkharej1
-ip -6 addr add 2002:480:1f10:e1f::3/64 dev 6to4_To_IR2
+ip -6 addr add 2009:480:1f10:e1f::2/64 dev 6to4_To_IR2
 ip link set 6to4_To_IR2 mtu 1480
 ip link set 6to4_To_IR2 up
 
-ip -6 tunnel add GRE6Tun_To_IR2 mode ip6gre remote 2002:480:1f10:e1f::4 local 2002:480:1f10:e1f::3
-ip addr add 10.10.10.4/30 dev GRE6Tun_To_IR2
+ip -6 tunnel add GRE6Tun_To_IR2 mode ip6gre remote 2009:480:1f10:e1f::1 local 2009:480:1f10:e1f::2
+ip addr add 10.10.11.2/30 dev GRE6Tun_To_IR2
 ip link set GRE6Tun_To_IR2 mtu 1436
 ip link set GRE6Tun_To_IR2 up
+
+exit 0
 EOL
             )
 
