2e9a3e78d307ab4d7346621b3e4d8fe51a92032d
ZedSoftOfficial <132583827+ZedSoftOfficial@users.noreply.github.com>
2024-08-24 15:58:11 +0330

    Update nes.sh

--- nes.sh
+++ nes.sh
@@ -377,9 +377,10 @@ EOF
         ;;
     4)
         # فعال‌سازی BBR
-        echo "Activating BBR..."
-        sudo sysctl -w net.ipv4.tcp_congestion_control=bbr
-        echo "BBR activated."
+        wget --no-check-certificate -O /opt/bbr.sh https://github.com/teddysun/across/raw/master/bbr.sh
+        chmod 755 /opt/bbr.sh
+        /opt/bbr.sh
+        echo "BBR optimization enabled."
         ;;
     5)
         # رفع مشکل زمان Whatsapp
