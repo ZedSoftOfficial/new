fd597401a37bd206517c31cc7b2ffc271bde55c4
ZedSoftOfficial <132583827+ZedSoftOfficial@users.noreply.github.com>
2024-08-24 15:39:27 +0330

    Update new.sh

--- new.sh
+++ new.sh
@@ -197,12 +197,14 @@ case $server_choice in
         echo "3) Iran2"
         read -p "Select an option (1-3): " server_option
 
-    if [ "$server_option" -eq 1 ]; then
+    elif [ "$server_option" -eq 2 ]; then
+        # برای سرور Iran1
         read -p "Enter the IP Outside: " ipkharej1
         read -p "Enter the IP Iran1: " ipiran1
         read -p "Enter the IP Iran2: " ipiran2
 
-        commands=$(cat <<EOF
+        # ایجاد محتوای جدید برای فایل rc.local
+        cat <<EOL > /etc/rc.local
 ip tunnel add 6to4_To_IR1 mode sit remote $ipiran1 local $ipkharej1
 ip -6 addr add 2002:480:1f10:e1f::2/64 dev 6to4_To_IR1
 ip link set 6to4_To_IR1 mtu 1480
@@ -222,12 +224,12 @@ ip -6 tunnel add GRE6Tun_To_IR2 mode ip6gre remote 2009:480:1f10:e1f::1 local 20
 ip addr add 10.10.11.2/30 dev GRE6Tun_To_IR2
 ip link set GRE6Tun_To_IR2 mtu 1436
 ip link set GRE6Tun_To_IR2 up
-EOF
 
-            )
+exit 0
+EOL
 
-        setup_rc_local "$commands"
-        echo "Configuration for Outside saved to /etc/rc.local and the file has been made executable."
+        chmod +x /etc/rc.local
+        echo "Configuration for Iran1 saved to /etc/rc.local and the file has been made executable."
 
     elif [ "$server_option" -eq 2 ]; then
         # برای سرور Iran1
